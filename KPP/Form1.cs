9e120ab46811e9a397eba55784a7b3b19c8cfc0f
Tommy Hagberg <tommy.hagberg@vgregion.se>
2023-09-19 13:34:53 +0200

    ny

--- KPP/Form1.cs
+++ KPP/Form1.cs
@@ -23,7 +23,7 @@ namespace KPP
         private void button1_Click(object sender, EventArgs e)
         {
             String Sql;
-            StreamWriter sw = new StreamWriter("KPP.txt");
+            StreamWriter sw = new StreamWriter("KPP_PRODUKTIONSDATABAS.txt");
             if (Datacontainer.connectsource == "Data Source=Klingen-su-db,62468; Initial Catalog = Klingen;")
             {
                 //  Sql = "SELECT ROW_NUMBER() OVER(ORDER BY[Index] Desc) AS RowNumber,[Index],[Personal number],[Familyname],[First Name] FROM[Klingen].[dbo].[Patients] WHERE[Personal number] IN(SELECT[Personal number] FROM[Klingen].[dbo].[Patients] GROUP BY[Personal number] HAVING COUNT(*) > 1)";
@@ -34,8 +34,9 @@ namespace KPP
             }
             else
             {
-                Sql = "SELECT ROW_NUMBER() OVER(ORDER BY[Index] Desc) AS RowNumber,[Index],[Personal number],[Familyname],[First Name] FROM[Klingen_test].[dbo].[Patients] WHERE[Personal number] IN(SELECT[Personal number] FROM[Klingen_test].[dbo].[Patients] GROUP BY[Personal number] HAVING COUNT(*) > 1)";
-
+                Sql = "WITH CombinedInvoice AS (SELECT* FROM [Klingen_test].dbo.[Internal Invoice Amnion] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice Blood] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice Chorion] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice Counselling] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice DNA] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice Fibroblast] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice FISH] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice Foetus] UNION ALL SELECT* FROM [Klingen_test].dbo.[Internal Invoice Tumor])";
+                Sql += "SELECT '2258' AS FilKod,'' AS PostID, FORMAT(GETDATE(), 'yyyy-MM-dd') AS RapportDatum,'2258' AS Saljare,'51001' AS SjukhusKod,REPLACE(C.[Personal number], '-', '') AS Pnr,LEFT(C.[Personal number], 2) AS Sekel,FORMAT(C.[Arrived date], 'yyyy-MM-dd') AS Handelsedatum,C.CustomerID, '' AS VDTID,'' AS VRDTyp,'L25' as Kostnadskod,1 AS Antal,FORMAT(C.Price, '0') AS Kostnad,'' AS Filler1,FORMAT(C.[Care Date], 'yyyy-MM-dd') AS Bestallningsdatum,'' AS Analyskod,'' AS Filler2,C.Type AS Analystext FROM CombinedInvoice C;";
+                //--Extract and transform the data
             }
             Datacontainer.command = new SqlCommand(Sql, Datacontainer.cnn);
             Datacontainer.command.CommandType = CommandType.Text;
@@ -122,7 +123,13 @@ namespace KPP
 
             reader.Close();
             sw.Close();
-            string message = "Utläsning klart !";
+            FileInfo file = new FileInfo("KPP_PRODUKTIONSDATABAS.txt");
+            if (Datacontainer.connectsource == "Data Source=Klingen-test-su-db,62468; Initial Catalog = Klingen_Test;")
+            {
+                file.MoveTo("KPP_TESTDATABAS.txt");
+            }
+          
+                string message = "Utläsning klart !";
             string title = "";
             MessageBoxButtons buttons = MessageBoxButtons.OK;
             DialogResult result = MessageBox.Show(message, title, buttons);
