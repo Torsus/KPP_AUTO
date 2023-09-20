089440a47af6908bb7a73e70250bca4d8ae9cc8e
tomha5 <tomha5@W350501>
2023-09-20 08:51:18 +0200

    ny

--- KPP/Form1.Designer.cs
+++ KPP/Form1.Designer.cs
@@ -44,17 +44,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(345, 9);
+            this.label1.Location = new System.Drawing.Point(460, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.Size = new System.Drawing.Size(35, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "KPP";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(344, 268);
+            this.button1.Location = new System.Drawing.Point(459, 330);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.Size = new System.Drawing.Size(128, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Skapa CSV-FIL";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,9 +65,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(345, 36);
+            this.label2.Location = new System.Drawing.Point(460, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.Size = new System.Drawing.Size(147, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Logga först in på ADB";
             // 
@@ -73,9 +76,10 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(348, 66);
+            this.radioButton1.Location = new System.Drawing.Point(464, 81);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(131, 17);
+            this.radioButton1.Size = new System.Drawing.Size(171, 21);
             this.radioButton1.TabIndex = 3;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Produktionsdatabasen";
@@ -85,9 +89,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(348, 105);
+            this.radioButton2.Location = new System.Drawing.Point(464, 129);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(96, 17);
+            this.radioButton2.Size = new System.Drawing.Size(124, 21);
             this.radioButton2.TabIndex = 4;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Testdatabasen";
@@ -96,9 +101,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(344, 211);
+            this.button2.Location = new System.Drawing.Point(459, 260);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 34);
+            this.button2.Size = new System.Drawing.Size(123, 42);
             this.button2.TabIndex = 5;
             this.button2.Text = "Logga in";
             this.button2.UseVisualStyleBackColor = true;
@@ -106,41 +112,47 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(348, 128);
+            this.textBox1.Location = new System.Drawing.Point(464, 158);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(348, 174);
+            this.textBox2.Location = new System.Drawing.Point(464, 214);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(278, 128);
+            this.label3.Location = new System.Drawing.Point(371, 158);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.Size = new System.Drawing.Size(87, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Användar-ID";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(278, 174);
+            this.label4.Location = new System.Drawing.Point(371, 214);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.Size = new System.Drawing.Size(47, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "Lösen";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(523, 270);
+            this.button3.Location = new System.Drawing.Point(697, 332);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 10;
             this.button3.Text = "Avsluta";
             this.button3.UseVisualStyleBackColor = true;
@@ -148,9 +160,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 303);
+            this.ClientSize = new System.Drawing.Size(1044, 373);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -162,6 +174,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
